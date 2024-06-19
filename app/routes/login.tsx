bbdd79e30a7c442e10b7c58ed7191106f79bc74f
Dimitri Gedda <dimitri.gedda@dgeddadevelops.me>
2024-06-19 15:35:01 -0400

    updates

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -85,79 +85,129 @@ export default function LoginPage() {
     passwordRef.current && (passwordRef.current.value = "bulbasaur");
   };
   return (
-    <div className="flex min-h-full flex-col justify-center">
-      <div className="relative">
-        <button
-          onClick={populateTestUser}
-          className=" bg-yellow-200 absolute mx-20 -my-[60px]"
-        >
-          Populate Test User
-        </button>
-        <div className="mx-auto w-full max-w-md px-8">
-          <Form method="post" className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email address
-              </label>
-              <div className="mt-1">
-                <input
-                  ref={emailRef}
-                  id="email"
-                  required
-                  // eslint-disable-next-line jsx-a11y/no-autofocus
-                  autoFocus={true}
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  aria-invalid={actionData?.errors?.email ? true : undefined}
-                  aria-describedby="email-error"
-                  className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                />
-                {actionData?.errors?.email ? (
-                  <div className="pt-1 text-red-700" id="email-error">
-                    {actionData.errors.email}
-                  </div>
-                ) : null}
-              </div>
-            </div>
+    <div className="flex min-h-full flex-col justify-center w-full">
+      <div className="flex justify-around">
+        <div className="bg-gray-200 mt-[5px] leading-5 w-full max-w-[600px] px-[40px] py-4">
+          <p className="mt-3 text-justify text-slate-900">
+            Anatomon Trainers serves as a centralized development environment,
+            offering direct views into the diverse workflows linking a full
+            stack JavaScript application and cloud computing services.
+          </p>
+          <div className="mt-3 text-justify">
+            <p className="mb-1">
+              Between Application and Cloudflare it provides visibility into:
+            </p>
+            <ul className="list-disc ml-6">
+              <li className="mb-2">Authorization</li>
+              <li className="mb-2">Image Optimization</li>
+              <li className="mb-2">Caching Directives</li>
+              <li className="mb-2">Leveraging a CDN</li>
+            </ul>
+          </div>
+          <p className="mt-3 text-justify">
+            Within the Cloudflare ecosystem it provides visibility into the
+            connections among a Cloudflare Worker, Object Storage and Key/Value
+            Cache, highlighting the decisions made in organizing and
+            implementing these relationships.
+          </p>
 
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div className="mt-1">
-                <input
-                  id="password"
-                  ref={passwordRef}
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  aria-invalid={actionData?.errors?.password ? true : undefined}
-                  aria-describedby="password-error"
-                  className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                />
-                {actionData?.errors?.password ? (
-                  <div className="pt-1 text-red-700" id="password-error">
-                    {actionData.errors.password}
-                  </div>
-                ) : null}
-              </div>
-            </div>
-
-            <input type="hidden" name="redirectTo" value={redirectTo} />
+          <p className="mt-3 text-justify">
+            Additionally a CI/CD pipeline via GitHub Actions deploying updates
+            to Fly.io provides a space for the quick implementation of
+            functional and end to end testing with Cypress.
+          </p>
+          <p className="mt-3 text-justify">
+            <span>Behind the youthful interface and study app,</span>{" "}
+            <span className="underline decoration-dotted decoration-gray-500">
+              Anatomon Trainers is a set of &quot;platforms&quot; allowing me to
+              isolate each of the aformentioned workflows to brainstorm,
+              implement, debug and test features, serving as a springboard to
+              address comparable issues or challenges within production
+              environments.
+            </span>{" "}
+          </p>
+        </div>
+        <div className="relative min-w-80">
+          <h3 className="mx-auto w-full max-w-md px-8 text-[22px] font-extrabold mt-[136px] tracking-tighter">
+            FOR DEMO PURPOSES
+          </h3>
+          <div className="mx-8 mt-1">
+            <span>Click to: &#8594;</span>
             <button
-              type="submit"
-              className="w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+              className=" bg-yellow-200 ml-2"
+              onClick={populateTestUser}
             >
-              Log in
+              Populate Test User
             </button>
-          </Form>
+          </div>
+          <div className="mx-auto w-full max-w-md px-8 mt-[20px]">
+            <Form method="post" className="space-y-6">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    ref={emailRef}
+                    id="email"
+                    required
+                    // eslint-disable-next-line jsx-a11y/no-autofocus
+                    autoFocus={true}
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    aria-invalid={actionData?.errors?.email ? true : undefined}
+                    aria-describedby="email-error"
+                    className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+                  />
+                  {actionData?.errors?.email ? (
+                    <div className="pt-1 text-red-700" id="email-error">
+                      {actionData.errors.email}
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    ref={passwordRef}
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    aria-invalid={
+                      actionData?.errors?.password ? true : undefined
+                    }
+                    aria-describedby="password-error"
+                    className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+                  />
+                  {actionData?.errors?.password ? (
+                    <div className="pt-1 text-red-700" id="password-error">
+                      {actionData.errors.password}
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+
+              <input type="hidden" name="redirectTo" value={redirectTo} />
+              <button
+                type="submit"
+                className="w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+              >
+                Log in
+              </button>
+            </Form>
+          </div>
         </div>
       </div>
     </div>
